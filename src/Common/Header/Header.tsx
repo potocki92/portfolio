1389f2aa7c5d8c808dac02af72a72384fc855fdf
potocki92 <mateusz.potocki92@gmail.com>
2023-12-28 22:12:49 +0100

    refactor: remove glass effect container and related references
    - Remove the <div> for the glass effect container from the JSX
    - Remove the related useRef and state (glassEffectContainerRef, isGlassEffectActive)
    - Refactor the useEffects for header height and glass effect animation
    - Simplify and clean up the code related to glass effect

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -15,126 +15,105 @@ import { animate } from "../../utils/animationOptions";
  * @returns {JSX.Element} The JSX representation of the header component.
  */
 const Header = (): JSX.Element => {
-// Refs...
-const headerRef = useRef<HTMLHeadElement>(null);
-const wrapperRef = useRef<HTMLDivElement>(null);
-const logoRef = useRef<HTMLDivElement>(null);
-const heroHeading = useRef<HTMLSpanElement>(null);
+  // Refs...
+  const headerRef = useRef<HTMLHeadElement>(null);
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const logoRef = useRef<HTMLDivElement>(null);
+  const heroHeading = useRef<HTMLSpanElement>(null);
 
   // Current height of the <Logo> element, tracked using the useCurrentHeight hook.
   const currentHeight = useCurrentHeight(logoRef);
-  
+
   /**
    * Current scroll position obtained using the useScroll hook.
    * @type {number}
-  */
- const scrollPosition: number = useScroll();
- 
- /**
-  * Minimum header height adjusted for content wrapped in the <div> element.
-  * @type {number}
- */
-const minHeaderHeight: number = 74 // Default height for minHeaderHeight
+   */
+  const scrollPosition: number = useScroll();
 
-/**
- * Maximum header height based on device information.
- * @type {number}
-*/
-const maxHeaderHeight: number = deviceHeightInfo();
-
-//State indicating whether the header is larger than a specified height.
-const [isHeaderLarger, setIsHeaderLarger] = useState<boolean>(false);
-
-/**
- * Calculated header height based on the scroll position.
- * @type {number}
-*/
-const headerHeight: number = Math.max(
-  minHeaderHeight,
-  maxHeaderHeight - Math.min(scrollPosition, maxHeaderHeight),
-  );
-const [targetTop, setTargetTop] = useState<number>(Math.max(-minHeaderHeight / 2 + headerHeight / 2))
-  
   /**
-   * Flag indicating if the current header height is equal to the minimum header height.
-   * @type {boolean}
-  */
- const isMinHeaderHeight: boolean = headerHeight === minHeaderHeight;
- 
- //Reference to the glass effect container used in animations.
- const { glassEffectContainerRef, isGlassEffectActive } = useGlassEffect({
-   isActive: isMinHeaderHeight,
-   duration: 0.5,
-  });
-  
-// Effect to update the state based on changes in the logo height.
-useEffect(() => {
-  if (logoRef.current) {
-    const isLarger = headerHeight > minHeaderHeight && headerHeight > currentHeight;
-    setIsHeaderLarger(isLarger);
-  }
-}, [headerHeight, currentHeight]);
+   * Minimum header height adjusted for content wrapped in the <div> element.
+   * @type {number}
+   */
+  const minHeaderHeight: number = 74; // Default height for minHeaderHeight
 
-// Effect to animate the glass effect and header height changes based on changes in scrollPosition.
-useEffect(() => {
-  gsap.to(
-    headerRef.current,
-    animate({
-      height: `${headerHeight}px`,
-    })
+  /**
+   * Maximum header height based on device information.
+   * @type {number}
+   */
+  const maxHeaderHeight: number = deviceHeightInfo();
+
+  //State indicating whether the header is larger than a specified height.
+  const [isHeaderLarger, setIsHeaderLarger] = useState<boolean>(false);
+
+  /**
+   * Calculated header height based on the scroll position.
+   * @type {number}
+   */
+  const headerHeight: number = Math.max(
+    minHeaderHeight,
+    maxHeaderHeight - Math.min(scrollPosition, maxHeaderHeight),
+  );
+  const [targetTop, setTargetTop] = useState<number>(
+    Math.max(-minHeaderHeight / 2 + headerHeight / 2),
   );
 
-  const updatedTargetTop = Math.max(-minHeaderHeight / 2 + headerHeight / 2);
-  setTargetTop(updatedTargetTop);
+  // Effect to update the state based on changes in the logo height.
+  useEffect(() => {
+    if (logoRef.current) {
+      const isLarger =
+        headerHeight > minHeaderHeight && headerHeight > currentHeight;
+      setIsHeaderLarger(isLarger);
+    }
+  }, [headerHeight, currentHeight]);
 
-  gsap.to(
-    wrapperRef.current,
-    animate({
-      top: `${targetTop / 1.5}px`,
-    }),
-  );
-}, [scrollPosition]);
+  // Effect to animate the glass effect and header height changes based on changes in scrollPosition.
+  useEffect(() => {
+    gsap.to(
+      headerRef.current,
+      animate({
+        height: `${headerHeight}px`,
+      }),
+    );
 
-// Effect to animate the opacity of the glassEffectContainer based on the state of isMinHeaderHeight.
-useEffect(() => {
-  gsap.to(
-    glassEffectContainerRef.current,
-    animate({
-      opacity: isMinHeaderHeight ? 1 : 0,
-    }),
-  );
-}, [isMinHeaderHeight]);
+    const updatedTargetTop = Math.max(-minHeaderHeight / 2 + headerHeight / 2);
+    setTargetTop(updatedTargetTop);
 
+    gsap.to(
+      wrapperRef.current,
+      animate({
+        top: `${targetTop / 1.5}px`,
+      }),
+    );
+  }, [scrollPosition]);
 
-// Effect to handle font size changes based on the state of isHeaderLarger.
-useEffect(() => {
-  gsap.to(
-    [logoRef.current],
-    animate({
-      fontSize: !isHeaderLarger ? "4vw" : "18vw",
-    }),
-  );
+  // Effect to handle font size changes based on the state of isHeaderLarger.
+  useEffect(() => {
+    gsap.to(
+      [logoRef.current],
+      animate({
+        fontSize: !isHeaderLarger ? "4vw" : "18vw",
+      }),
+    );
 
-  gsap.to(
-    heroHeading.current,
-    animate({
-      fontSize: !isHeaderLarger ? "1.5vw" : "7vw",
-    }),
-  );
-}, [isHeaderLarger]);
-
+    gsap.to(
+      heroHeading.current,
+      animate({
+        fontSize: !isHeaderLarger ? "1.5vw" : "7vw",
+      }),
+    );
+  }, [isHeaderLarger]);
 
   return (
     <header ref={headerRef} {...stylex.props(styles.header)}>
       <div
         ref={wrapperRef}
         className="w-full py-3"
-        style={{ position: 'fixed', left: '50%', transform: 'translateX(-50%)' }}
+        style={{
+          position: "fixed",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
       >
-        <div
-          ref={glassEffectContainerRef}
-          {...stylex.props(styles.glassEffectContainer(headerHeight))}
-        ></div>
         <Logo ref={logoRef} />
         <span ref={heroHeading} {...stylex.props(styles.heroHeading)}>
           FULLSTACK DEVELOPER
