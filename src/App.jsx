2409e438dc56224d524bad790c288c96fd19dd23
potocki92 <mateusz.potocki92@gmail.com>
2023-12-29 23:16:46 +0100

    style: apply stylex to App component
    - Imported stylex library.
    - Created a style object using stylex.create for the base styles.
    - Applied the base styles to the App component using stylex.props.
    - Removed the unnecessary <div> wrapping.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import * as stylex from "@stylexjs/stylex";
 import Layout from "./Layout/Layout";
 
+const styles = stylex.create({
+  base: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  }
+})
 function App() {
   return (
-    <div>
+    <div {...stylex.props(styles.base)}>
       <Layout/>
     </div>
   );
