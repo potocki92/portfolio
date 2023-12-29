2a662315dcabf5fa1166a6b98ea2742088a37fb5
potocki92 <mateusz.potocki92@gmail.com>
2023-12-29 23:18:01 +0100

    style: Update Nav component styles
    - Adjusted the Nav component styles to make them more flexible and responsive.
    - Used `stylex` for styling, passing the `height` prop dynamically.
    - Replaced hard-coded values with dynamic styles based on the `stylex` styles.

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,13 +1,34 @@
+import * as stylex from "@stylexjs/stylex";
 import { FC } from "react";
 
-const Nav: FC = () => {
+const styles = stylex.create({
+    nav: (height?) => ({
+        height: 40,
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        backgroundColor: "rgba(39,39,42,.9)",
+        borderRadius: "9999px",
+        padding: "0 0.75rem",
+        border: "1px solid hsla(0,0%,100%,.1)",
+        color: "rgba(228, 228, 231)"
+    }),
+    list: {
+        padding: "0.5rem 0.75rem"
+    }
+})
+
+const Nav: FC<{height: number}> = ({height}) => {
+    console.log(height);
+    
     return (
-        <nav className="px-[60px] flex items-center justify-between top-0 left-0 right-0">
-            <ul className="relative hidden md:flex gap-6">
-                <li>
-                    <a>about</a></li>
-                <li><a>projects</a></li>
-                <li><a>contact</a></li>
+        <nav {...stylex.props(styles.nav(height))}>
+            <ul className="relative hidden md:flex gap-6 text-sm">
+                <li {...stylex.props(styles.list)}>
+                    <a>Home</a></li>
+                <li {...stylex.props(styles.list)}><a>Creating</a></li>
+                <li {...stylex.props(styles.list)}><a>Resume</a></li>
+                <li {...stylex.props(styles.list)}><a>Contact</a></li>
             </ul>
         </nav>
     )
