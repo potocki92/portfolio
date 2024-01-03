441ea37c05f59ddb9030c8fba4025132c300c021
potocki92 <mateusz.potocki92@gmail.com>
2024-01-03 13:00:55 +0100

    style: update Wrapper component
    - Added import statements for framer-motion, stylex, and deviceHeightInfo.
    - Defined `maxHeight` using `deviceHeightInfo()`.
    - Used `useViewportScroll` and `useTransform` from framer-motion to create an `initialY` animation.
    - Applied the `initialY` animation to the `top` property of the style.

--- src/Components/Wrapper/Wrapper.tsx
+++ src/Components/Wrapper/Wrapper.tsx
@@ -1,20 +1,39 @@
 import { ReactNode, forwardRef } from "react";
-import * as stylex from "@stylexjs/stylex"
+import { motion, useViewportScroll, useTransform } from "framer-motion";
+import * as stylex from "@stylexjs/stylex";
+import deviceHeightInfo from "../../utils/deviceHeightInfo";
 
 type WrapperProps = {
-    children?: ReactNode,
-    style?: stylex.StyleXStyles
-}
-const Wrapper = forwardRef<HTMLDivElement, WrapperProps>((props, ref) => {
-    const { children, style } = props
-    return (
-        <div
-            ref={ref}
-            {...stylex.props(style)}
-            >
-                {children}
-        </div>
-    )
-})
+  children?: ReactNode;
+  style?: stylex.StyleXStyles;
+};
 
-export default Wrapper
+const maxHeight: number = deviceHeightInfo();
+
+const Wrapper = forwardRef<HTMLDivElement, WrapperProps>((props, ref) => {
+  const { children, style } = props;
+
+  const { scrollY } = useViewportScroll();
+  const initialY = useTransform(
+    scrollY,
+    [0, 500],
+    [`${maxHeight / 3}px `, `25px`],
+  );
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{
+        ...stylex.props(style),
+        position: "fixed",
+        left: "50%",
+        top: initialY,
+        transform: "translate(-50%)",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+});
+
+export default Wrapper;
