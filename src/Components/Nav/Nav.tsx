93cff15a7cef6a2859a0c447ec3ee5f4e5eac7f6
potocki92 <mateusz.potocki92@gmail.com>
2024-01-04 13:20:13 +0100

    feat: implement dynamic navigation items with active state
    - Refactor Nav component to use a dynamic array for navigation labels
    - Add state for tracking the active index
    - Implement dynamic rendering of navigation items with hover and active states
    - Update styles to reflect the active state
    - Remove redundant onChange function

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,20 +1,31 @@
 import * as stylex from "@stylexjs/stylex";
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import styles from "./Nav.stylex";
 
+const Nav: FC<{}> = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(0);
 
-const Nav: FC<{height?: number}> = ({height}) => {
-    return (
-        <nav {...stylex.props(styles.nav)}>
-            <ul className="relative hidden md:flex gap-6 text-sm">
-                <li {...stylex.props(styles.list)}>
-                    <a>Home</a></li>
-                <li {...stylex.props(styles.list)}><a>Creating</a></li>
-                <li {...stylex.props(styles.list)}><a>Resume</a></li>
-                <li {...stylex.props(styles.list)}><a>Contact</a></li>
-            </ul>
-        </nav>
-    )
-}
+  const onChange = (index: number) => {
+    setActiveIndex(index);
+  };
+  return (
+    <nav {...stylex.props(styles.nav)}>
+      <ul className="relative hidden md:flex gap-6 text-sm">
+        {["Home", "Creating", "Resume", "Contact"].map((label, index) => (
+          <li
+            key={index}
+            {...stylex.props(
+              styles.item,
+              activeIndex === index && styles.activeItem,
+            )}
+            onClick={() => onChange(index)}
+          >
+            <a>{label}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
