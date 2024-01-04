4611e0206fa9dc3875e5de1b412affe979580d1f
potocki92 <mateusz.potocki92@gmail.com>
2024-01-04 12:14:24 +0100

    refactor: utilize external style module for Nav component
    - Remove inline styles from the Nav component
    - Import styles from the external "./Nav.stylex" module
    - Update the usage of styles in the Nav component

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,26 +1,9 @@
 import * as stylex from "@stylexjs/stylex";
 import { FC } from "react";
+import styles from "./Nav.stylex";
 
-const styles = stylex.create({
-    nav: ({
-        position: "fixed",
-        height: 40,
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        backgroundColor: "rgba(39,39,42,.9)",
-        borderRadius: "9999px",
-        padding: "0 0.75rem",
-        border: "1px solid hsla(0,0%,100%,.1)",
-        color: "rgba(228, 228, 231)"
-    }),
-    list: {
-        padding: "0.5rem 0.75rem"
-    }
-})
 
 const Nav: FC<{height?: number}> = ({height}) => {
-    
     return (
         <nav {...stylex.props(styles.nav)}>
             <ul className="relative hidden md:flex gap-6 text-sm">
