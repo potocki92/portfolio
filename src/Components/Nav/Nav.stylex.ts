f6f82805278c00b51f389da8955a5500d7d87726
potocki92 <mateusz.potocki92@gmail.com>
2024-01-04 13:19:51 +0100

    style: update styles for Nav component
    - Refactor styles using stylex.create for better readability
    - Add styles for list items including hover and active states

--- src/Components/Nav/Nav.stylex.ts
+++ src/Components/Nav/Nav.stylex.ts
@@ -1,21 +1,31 @@
 import * as stylex from "@stylexjs/stylex";
 
 const styles = stylex.create({
-    nav: {
-        position: "fixed",
-        height: 40,
-        display: "flex",
-        justifyContent: "flex-end",
-        alignItems: "center",
-        backgroundColor: "rgba(39, 39, 42, 0.9)",
-        borderRadius: "9999px",
-        padding: "0 0.75rem",
-        border: "1px solid rgba(255, 255, 255, 0.1)",
-        color: "rgba(228, 228, 231)",
-      },
-      
-    list: {
-        padding: "0.5rem 0.75rem"
-    }
-})
-export default styles
+  nav: {
+    position: "fixed",
+    height: 40,
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "rgba(39, 39, 42, 0.9)",
+    borderRadius: "9999px",
+    padding: "0 0.75rem",
+    border: "1px solid rgba(255, 255, 255, 0.1)",
+    color: "rgba(228, 228, 231)",
+  },
+
+  item: {
+    cursor: "pointer",
+    padding: "0.5rem 0.75rem",
+    ":hover": {
+      color: "rgba(251 37 118/1)",
+    },
+    userSelect: "none",
+    transitionTimingFunction: "cubic-bezier(.4,0,.2,1)",
+    transitionDuration: ".15s",
+  },
+  activeItem: {
+    color: "rgba(251 37 118/1)",
+  },
+});
+export default styles;
