9d51acc040e17b1c554147344df21e2d535f3a99
potocki92 <mateusz.potocki92@gmail.com>
2024-01-04 12:12:02 +0100

    refactor: update Header component with framer-motion controls for scroll-based animations
    - Imported `useAnimation` from `framer-motion`.
    - Refactored `scrollY.onChange` event handler to use `controls` for handling visibility animations.
    - Updated `Header` component structure to include `motion.header` for animation control.

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -1,26 +1,53 @@
-import { motion, useViewportScroll, useTransform } from "framer-motion";
+import { motion, useViewportScroll, useTransform, useAnimation } from "framer-motion";
 import Logo from "../../Components/Logo/Logo";
 import * as stylex from "@stylexjs/stylex";
 import styles from "./Header.stylex";
 import Wrapper from "../../Components/Wrapper/Wrapper";
 import Nav from "../../Components/Nav/Nav";
+import { useRef } from "react";
+import motionValueScrollYFactory from "../../utils/motionValueScroll";
+
 /**
  * Header component representing the header of a webpage with dynamic animations and styling.
- *
- * @component
- * @returns {JSX.Element} The JSX representation of the header component.
- */
+*
+* @component
+* @returns {JSX.Element} The JSX representation of the header component.
+*/
 const Header = (): JSX.Element => {
-
+  const controls = useAnimation();
+  const delta = useRef(0);
+  const lastScrollY = useRef(0);
   const { scrollY } = useViewportScroll();
-  const initialYLogo = useTransform(
-    scrollY,
-    [0, 500],
-    ["clamp(1.3rem, 18vw, 18rem)", "clamp(1.3rem, 3.8vw, 18rem)"],
-  );
-  const initialYSpan = useTransform(scrollY, [0, 500], ["clamp(0.5rem, 6.6vw, 6.7rem)", "clamp(0.5rem, 1.5vw, 6.7rem)"]);
+  // Define initial values for logo font size based on scroll position.
+  const initialYLogo = motionValueScrollYFactory(scrollY,["clamp(1.3rem, 18vw, 18rem)", "clamp(1.3rem, 2vw, 18rem)"]);
+  // Define initial values for hero heading font size based on scroll position.
+  const initialYSpan = motionValueScrollYFactory(scrollY,["clamp(0.5rem, 6.6vw, 6.7rem)", "clamp(0.49rem, 0.75vw, 6.7rem)"]);
+
+  // This event handler is triggered whenever the scroll position changes.
+  scrollY.onChange((val) => {
+    const diff = Math.abs(val - lastScrollY.current);
+    if (val >= lastScrollY.current) {
+      delta.current = delta.current >= 10 ? 10 : delta.current + diff;
+    } else {
+      delta.current = delta.current <= -10 ? -10 : delta.current - diff;
+    }
+    if (delta.current >= 10 && val > 650) {
+      controls.start("hidden");
+    } else if (delta.current <= -10 || val < 200) {
+      controls.start("visible");
+    }
+    lastScrollY.current = val;
+  });
+
   return (
-    <header {...stylex.props(styles.header)}>
+    <motion.header 
+    initial="visible"
+      animate={controls}
+      variants={{
+        visible: { top: "0px" },
+        hidden: { top: "-100px" }
+      }}
+    {...stylex.props(styles.header)}>
       <Wrapper style={styles.headerWrapper}>
         <motion.div
           style={{
@@ -38,7 +65,7 @@ const Header = (): JSX.Element => {
         </motion.div>
       </Wrapper>
       <Nav />
-    </header>
+    </motion.header>
   );
 };
 
