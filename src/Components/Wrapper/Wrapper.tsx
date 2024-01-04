7224bc8a411d3646fa4d4e89cb4616f329639878
potocki92 <mateusz.potocki92@gmail.com>
2024-01-04 12:16:28 +0100

    refactor: improve Wrapper component with dynamic styling based on scroll position
    - Simplify import statements by grouping related imports
    - Replace forwardRef with a functional component signature for Wrapper
    - Utilize motionValueScrollYFactory for dynamic Y-axis position based on scroll
    - Add motionValueScrollYFactory for dynamic X-axis position based on scroll
    - Introduce initialTransform for initial transform values based on scroll position
    - Adjust styles in the motion.div element to use the dynamic initial values

--- src/Components/Wrapper/Wrapper.tsx
+++ src/Components/Wrapper/Wrapper.tsx
@@ -1,7 +1,8 @@
-import { ReactNode, forwardRef } from "react";
+import { ReactNode, forwardRef, useTransition } from "react";
 import { motion, useViewportScroll, useTransform } from "framer-motion";
 import * as stylex from "@stylexjs/stylex";
 import deviceHeightInfo from "../../utils/deviceHeightInfo";
+import motionValueScrollYFactory from "../../utils/motionValueScroll";
 
 type WrapperProps = {
   children?: ReactNode;
@@ -9,26 +10,45 @@ type WrapperProps = {
 };
 
 const maxHeight: number = deviceHeightInfo();
-
-const Wrapper = forwardRef<HTMLDivElement, WrapperProps>((props, ref) => {
+/**
+ * Functional React component representing a wrapper with dynamic styling based on scroll position.
+ *
+ * @param {WrapperProps} props - The properties of the Wrapper component.
+ * @returns {JSX.Element} The JSX representation of the Wrapper component.
+ */
+const Wrapper = ((props: WrapperProps) => {
   const { children, style } = props;
 
+  // Obtain scroll position information using framer-motion's useViewportScroll.
   const { scrollY } = useViewportScroll();
-  const initialY = useTransform(
-    scrollY,
-    [0, 500],
-    [`${maxHeight / 3}px `, `25px`],
-  );
+
+  // Define initial values for Y-axis position based on scroll position.
+  const initialY = motionValueScrollYFactory(scrollY, [
+    `${maxHeight / 3}px`,
+    `25px`,
+  ]);
+
+  // Define initial values for X-axis position based on scroll position.
+  const initialX = motionValueScrollYFactory(scrollY, [
+    `calc(50% + 0rem)`,
+    `calc(0% + 1.5rem)`,
+  ]);
+
+  // Define initial transform values based on scroll position.
+  const initialTransform = motionValueScrollYFactory(scrollY, [
+    `translate(-50%)`,
+    "translate(0%)",
+  ]);
 
   return (
     <motion.div
-      ref={ref}
+      id="wrapper"
       style={{
         ...stylex.props(style),
-        position: "fixed",
-        left: "50%",
+        position: "absolute",
+        left: initialX,
         top: initialY,
-        transform: "translate(-50%)",
+        transform: initialTransform,
       }}
     >
       {children}
