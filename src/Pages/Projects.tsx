984549b01f2f90f9577cf31f495f0fcea5e5b2bc
potocki92 <mateusz.potocki92@gmail.com>
2024-01-05 12:19:50 +0100

    feat: add Projects component with Helmet for SEO metadata
    - Create Projects component with a basic structure
    - Integrate Helmet and HelmetProvider from react-helmet-async for managing SEO metadata
    - Define title, description, canonical URL, and Open Graph metadata for the Projects page
    - Include a placeholder Section component within the Projects component

---
+++ src/Pages/Projects.tsx
@@ -0,0 +1,25 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import Section from "../Components/Section/Section";
+
+const Projects = () => {
+    const helmetTitle = 'Projects';
+    const helmetDescription = "Projects I've created while aiming to make my mark on the world.";
+
+    return (
+        <>
+            <HelmetProvider>
+                <Helmet>
+                    <title>{helmetTitle}</title>
+                    <meta name="description" content={helmetDescription} />
+                    <link rel="canonical" href="/projects"/>
+                    <meta property="og:title" content={helmetTitle} />
+                    <meta property="og:description" content={helmetDescription} />
+                </Helmet>
+            
+                <Section />
+            </HelmetProvider>
+        </>
+    )
+}
+
+export default Projects
