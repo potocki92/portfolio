ba3b482d47763caf5ab49a0eca0e222d5d4cd6c2
potocki92 <mateusz.potocki92@gmail.com>
2024-01-05 12:23:28 +0100

    chore: update Projects component structure and styling
    - Remove unnecessary tailwindClass prop from the Section component in Projects

--- src/Sections/Projects/Projects.tsx
+++ src/Sections/Projects/Projects.tsx
@@ -3,7 +3,7 @@ import Section from "../../Components/Section/Section"
 
 const Projects: FC = () => {
     return (
-        <Section id="projects" tailwindClass="relative flex w-full select-none items-center justify-center px-3 mb-8">
+        <Section id="projects">
             <div className="w-full flex flex-col gap-8">
                 <h2 className="text-5xl font-bold">Projects</h2>
             </div>
