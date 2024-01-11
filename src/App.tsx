f2e524a0598555198b421e14a4c2dbe2a812cba1
potocki92 <mateusz.potocki92@gmail.com>
2024-01-11 22:01:52 +0100

    chore: update project configuration and file names
    1. **Update Main File in index.html:**
       - Change the main file name in index.html from "main.jsx" to "index.tsx".
    
    2. **Update "@stylexjs/stylex" Version:**
       - Update "@stylexjs/stylex" version to "^0.4.1".
       - Configure "vite-plugin-stylex" to use "@stylexjs/babel-plugin" version "0.4.1".
    
    3. **Refactor App Component:**
       - Rename App.jsx to App.tsx for TypeScript compatibility.
       - Introduce global styling using stylex.
       - Implement a dark mode toggle button with a state and toggle function.
       - Apply global styling based on the dark mode state.
    
    4. **Update Main File in index.tsx:**
       - Change the main file name in index.js from "main.jsx" to "index.tsx".
    
    This commit enhances project organization, updates dependencies, and introduces global styling with a dark mode feature.

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,41 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Projects from "./Pages/Projects";
+import { globalTokens as $, colors } from "./styles/globalTokens.stylex";
+import { useState } from "react";
+import { dark } from "./styles/themes.stylex";
 
 const styles = stylex.create({
+  reset: {
+    minHeight: "100%",
+    margin: 0,
+    padding: 0,
+  },
+  body: {
+    backgroundColor: $.backgroundColor,
+    color: colors.primaryText
+  },
   base: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-  }
+  },
+   button: {
+    zIndex: 1000
+   }
 })
+
+
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+  const toggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
   return (
-    <div {...stylex.props(styles.base)}>
+    <div {...stylex.props( styles.base, darkMode ? styles.body : dark)}>
+       <button {...stylex.props(styles.button)} onClick={toggleDarkMode}>
+        Toggle Dark Mode
+      </button>
      <Routes>
         <Route path="/" element={<Layout />}>
           <Route
