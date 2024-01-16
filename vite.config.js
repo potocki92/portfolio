5dca09cefeede040bc1e1263d756b2bebd11c19a
potocki92 <mateusz.potocki92@gmail.com>
2024-01-16 12:40:35 +0100

    *

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import styleX from "vite-plugin-stylex";
 
 export default defineConfig({
-  base: "/portfolio/",
+  base: process.env.VITE_BASE_PATH || "/portfolio/",
   plugins: [react(), styleX()],
   server: {
     port: 3000
