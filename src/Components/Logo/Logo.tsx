ea7df69ad587224ee3f13a53ffdf05ff7015a280
potocki92 <40219157+potocki92@users.noreply.github.com>
2024-01-18 15:24:46 +0000

    chore: update project configuration
    1. **Update Package Lock:**
       - Upload the new `package-lock.json`.
    
    2. **Update Vite Plugin:**
       - Move `"@vitejs/plugin-react": "^4.2.1"` from `"dependencies"` to `"devDependencies"` in `package.json`.
    
    3. **Update TypeScript Config:**
       - Add `"typeRoots": ["node_modules/@types", "src/types"]` to `tsconfig.json`.
    
    4. **Vite Config Rename:**
       - Change the name of `vite.config.js` to `vite.config.ts`.
    
    5. **Remove GitHub Workflows:**
       - Delete the `.github/workflows` directory.
    
    6. **Add Image Module Declarations:**
       - Create `images.d.ts` to declare image modules.
    
    7. **Logo Update:**
       - Add the image from the assets folder to the `Logo` component in `Logo.tsx`.

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,6 +1,8 @@
 import type { StyleXStyles } from "@stylexjs/stylex";
 import * as stylex from "@stylexjs/stylex";
 import { globalTokens as $  } from "../../styles/globalTokens.stylex";
+import image from "../../assets/react.svg"
+
 type LogoProps = {
   style?: StyleXStyles;
 };
@@ -17,7 +19,7 @@ const Logo = (props: LogoProps) => {
   const { style } = props;
   return (
     <div id="logo" {...stylex.props(style)}>
-      <img {...stylex.props(styles.img)}/>
+      <img src={image} {...stylex.props(styles.img)}/>
     </div>
   );
 };
