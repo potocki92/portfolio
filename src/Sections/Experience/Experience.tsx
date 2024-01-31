1b9dd068eb3398a38ec995f843d056b4cf9c1803
potocki92 <mateusz.potocki92@gmail.com>
2024-01-31 13:05:58 +0100

    feat: Render experience data using Card component
    This commit updates the 'Experience' component to render experience data using the 'Card' component. It imports the 'Card' component and 'MyExperience' array from 'data/lifeApi'. The 'MyExperience' array contains example data for experiences. The 'Experience' component maps over this array to render each experience as a card, displaying its ti

--- src/Sections/Experience/Experience.tsx
+++ src/Sections/Experience/Experience.tsx
@@ -1,17 +1,30 @@
-import { Container } from "../../Components/Container/Container"
-import PageTitle from "../../Components/PageTitle/PageTitle"
-import Paragraph from "../../Components/Paragraph/Paragraph"
-import { ExperienceExtended, ExperienceTitle } from "../../data/lifeApi"
+import { Card } from "../../Components/Card/Card";
+import { Container } from "../../Components/Container/Container";
+import PageTitle from "../../Components/PageTitle/PageTitle";
+import { ExperienceTitle, MyExperience } from "../../data/lifeApi";
 
 const Experience = () => {
-    return (
-        <Container>
-            <div>
-                <PageTitle>{ExperienceTitle}</PageTitle>
-                <Paragraph>{ExperienceExtended}</Paragraph>
-            </div>
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <div>
+        {
+          // TODO: div as card component -> 'created new component'
+        }
+        <PageTitle>{ExperienceTitle}</PageTitle>
+        {MyExperience.map((Experience) => (
+            <Card key={Experience.title}>
+                <Card.Title>{Experience.title}</Card.Title>
+                <Card.Date>{Experience.date}</Card.Date>
+                <Card.Description>{Experience.description}</Card.Description>
+                <Card.Tech>{Experience.techStack.map((stack) => (
+                    <Card.TechItem key={stack}>{stack}</Card.TechItem>
+                ))}</Card.Tech>
+            </Card>
+        //   <Paragraph>{Experience.description}</Paragraph>
+        ))}
+      </div>
+    </Container>
+  );
+};
 
-export default Experience
+export default Experience;
