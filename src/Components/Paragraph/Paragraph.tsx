689fb11ab9159c67be95b9e026a5bb25516568f2
potocki92 <mateusz.potocki92@gmail.com>
2024-01-31 13:03:02 +0100

    style: Update Paragraph component
    This commit modifies the Paragraph component to accept an optional 'as' prop for specifying the HTML element to render. It also adjusts the color style to use the 'colors.secondText' token for consistency with the global style tokens.

--- src/Components/Paragraph/Paragraph.tsx
+++ src/Components/Paragraph/Paragraph.tsx
@@ -1,21 +1,20 @@
 import * as stylex from "@stylexjs/stylex";
-import { ReactNode } from "react";
 import { colors, text } from "../../styles/globalTokens.stylex";
 
-type Props = {
-  children?: ReactNode;
-};
 const styles = stylex.create({
   p: {
     marginTop: "1.5rem",
     fontSize: text.sm,
     lineHeight: "1.75rem",
     textWrap: "balance",
-    color: colors.secondText
+    color: colors.secondText,
   },
 });
-const Paragraph = ({ children }: React.PropsWithChildren<Props>) => {
-  return <p {...stylex.props(styles.p)}>{children}</p>;
+const Paragraph = ({
+  as: Component = "p",
+  children,
+}: React.PropsWithChildren<{ as?: React.ElementType }>) => {
+  return <Component {...stylex.props(styles.p)}>{children}</Component>;
 };
 
-export default Paragraph
+export default Paragraph;
