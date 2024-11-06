f8c1c83d3d704596e1b06b36e7df08cf2ac026dc
potocki92 <mateusz.potocki92@gmail.com>
2024-11-06 21:37:36 +0100

    style: update Hero component styling
    1. Modified container and containerGrid styles in Hero component.
    2. Added position, left, bottom and height.

--- src/Sections/Hero/Hero.tsx
+++ src/Sections/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-import '../../i18n';
+import "../../i18n";
 import * as stylex from "@stylexjs/stylex";
 import { globalTokens as $ } from "../../styles/globalTokens.stylex";
 import { Container } from "../../Components/Container/Container";
@@ -11,16 +11,21 @@ const styles = stylex.create({
   container: {
     width: "100%",
     maxWidth: $.maxTitleWidth,
+    position: "absolute",
+    left: `calc(${$.globalYPadding} + 95px)`,
+    bottom: "20px",
   },
   containerGrid: {
     gridArea: {
-      default: "1/1/1/3", 
-    }
-  }
+      default: "1/1/1/3",
+    },
+    height: "100px",
+    position: "relative",
+  },
 });
 
 const Hero = () => {
-  const {Name, HeroAbout} = LifeApiComponent();
+  const { Name, HeroAbout } = LifeApiComponent();
   return (
     <>
       <Container {...stylex.props(styles.containerGrid)}>
