af7521a0ba37415802214c2e40a0d78976e1499c
potocki92 <mateusz.potocki92@gmail.com>
2024-11-07 22:47:57 +0100

    refactor: update ToggleTheme component
    1. Simplified ToggleTheme component structure.
    2. Ensured proper type usage for `iconToggleSize`.
    3. Improved readability and maintainability.

--- src/Components/ToggleTheme/ToggleTheme.tsx
+++ src/Components/ToggleTheme/ToggleTheme.tsx
@@ -1,25 +1,22 @@
 import { StyleXStyles } from "@stylexjs/stylex";
-import Button from "../Button/Button"
+import Button from "../Button/Button";
 import { useTheme } from "../ThemeContext/ThemeContex";
 import { Sun, Moon } from "react-feather";
-const iconToggleSize: string =  "1rem" as const
+const iconToggleSize: string = "1rem" as const;
 type ToggleProps = {
-    style?: StyleXStyles,
-}
+  style?: StyleXStyles;
+};
 
 const ToggleTheme = ({}: React.PropsWithChildren<ToggleProps>) => {
   const { theme, toggleTheme } = useTheme();
-    
-    return (
-        <>
-            <Button
-                onClick={toggleTheme}
-                is={true}
-            >
-                {theme === 'light' ?  <Moon size={iconToggleSize}/> :<Sun size={iconToggleSize}/> }
-            </Button>
-        </>
-    )
-}
 
-export default ToggleTheme
+  return (
+    <>
+      <Button onClick={toggleTheme} is={true} aria-label="Toggle Theme">
+        {theme === "light" ? <Moon size={iconToggleSize} /> : <Sun size={iconToggleSize} />}
+      </Button>
+    </>
+  );
+};
+
+export default ToggleTheme;
