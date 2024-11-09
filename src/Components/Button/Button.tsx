840bbd6cd41779a74de2e519f70ccf3e7f578353
potocki92 <mateusz.potocki92@gmail.com>
2024-11-09 20:48:45 +0100

    refactor: optimize Button component with memo
    Wrapped Button component with `memo` for performance optimization.

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,5 +1,5 @@
 import * as stylex from "@stylexjs/stylex";
-import React, { ComponentPropsWithRef, ElementType, forwardRef } from "react";
+import React, { ComponentPropsWithRef, ElementType, forwardRef, memo } from "react";
 import { globalTokens as $, colors, globalStyles, text } from "../../styles/globalTokens.stylex";
 
 const styles = stylex.create({
@@ -26,7 +26,7 @@ const styles = stylex.create({
 
 /**
  * Props for the Button component.
- * 
+ *
  * @template T - The type of the component to render as.
  * @typedef {Object} Props
  * @property {T} [as] - The component to render as. Defaults to "button".
@@ -42,30 +42,34 @@ type Props<T extends ElementType> = {
 
 /**
  * A customizable button component that can render as different HTML elements.
- * 
+ *
  * @template T - The type of the component to render as.
  * @param {Props<T>} props - The props for the button component.
  * @param {React.Ref<T extends keyof JSX.IntrinsicElements ? JSX.IntrinsicElements[T] : T>} ref - The ref to the component.
  * @returns {JSX.Element} The rendered button component.
  */
-const Button = forwardRef(
-  <T extends ElementType = "button">(
-    { as: Component = "button", ...props }: Props<T>,
-    ref: React.Ref<T extends keyof JSX.IntrinsicElements ? JSX.IntrinsicElements[T] : T>,
-  ) => {
-    const { className, children, onClick, is, ...rest } = props;
-    return (
-      <Component
-        className={className}
-        onClick={onClick}
-        {...stylex.props(styles.button, is ? globalStyles.flex : globalStyles.flexButtons)}
-        {...rest}
-        ref={ref}
-      >
-        {children}
-      </Component>
-    );
-  },
+const Button = memo(
+  forwardRef(
+    <T extends ElementType = "button">(
+      { as: Component = "button", ...props }: Props<T>,
+      ref: React.Ref<T extends keyof JSX.IntrinsicElements ? JSX.IntrinsicElements[T] : T>,
+    ) => {
+      const { style, className, children, onClick, is, ...rest } = props;
+      console.log(props);
+      
+      return (
+        <Component
+          className={className}
+          onClick={onClick}
+          {...stylex.props(styles.button, is ? globalStyles.flex : globalStyles.flexButtons, style)}
+          {...rest}
+          ref={ref}
+        >
+          {children}
+        </Component>
+      );
+    },
+  ),
 );
 
 Button.displayName = "Button";
