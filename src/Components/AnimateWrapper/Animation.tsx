45a35c432033ab4dadf90300e52b06a651ce8b52
potocki92 <mateusz.potocki92@gmail.com>
2024-11-09 20:47:22 +0100

    feat: add onClick, memo and modal animations
    1. Added `onClick` handler to Animation component.
    2. Added `exit` style to StyleProps.
    3. Wrapped components with `memo` for performance optimization.
    4. Implemented `getBackdropAnimationConfig ` function for backdrop animations.
    5. Implemented `getModalAnimationConfig ` function for modalanimations.
    6. Added modal animations to Animation component.

--- src/Components/AnimateWrapper/Animation.tsx
+++ src/Components/AnimateWrapper/Animation.tsx
@@ -1,13 +1,16 @@
-import React, { ReactNode } from "react";
+import React, { memo, ReactNode } from "react";
 import { motion, MotionProps, MotionValue } from "framer-motion";
 import * as stylex from "@stylexjs/stylex";
 import { getHeaderAnimationConfig } from "./AnimationHeader";
 import { getAvatarAnimationConfig } from "./AnimationAvatar";
 import { getWrapperAnimationConfig } from "./AnimationWrapper";
+import { getBackdropAnimationConfig } from "./AnimationBackdrop";
+import { getModalAnimationConfig } from "./AnimationModal";
 
 interface StyleProps {
   style?: stylex.StyleXStyles;
   initial?: any;
+  exit?: any;
   initialX?: any;
   initialY?: any;
   initialTransform?: string | MotionValue<string>;
@@ -20,6 +23,7 @@ interface AnimateConfig {
   motion?: MotionProps;
   motionComponent?: React.ElementType;
   children?: ReactNode;
+  onClick?: any;
 }
 
 /**
@@ -30,6 +34,7 @@ class AnimateBuilder {
   private motion: MotionProps = {};
   private motionComponent: React.ElementType = motion.div;
   private children: ReactNode = null;
+  private onClick: any;
 
   /**
    * Sets the style properties.
@@ -72,6 +77,16 @@ class AnimateBuilder {
   }
 
   /**
+   * Sets the onClick event.
+   * @param {Function} onClick - The onClick event.
+   * @returns {AnimateBuilder} The builder instance.
+   */
+  setOnClick(onClick: any): AnimateBuilder {
+    this.onClick = onClick;
+    return this;
+  }
+
+  /**
    * Builds and returns the animated component.
    * @returns {JSX.Element} The animated component.
    */
@@ -82,11 +97,13 @@ class AnimateBuilder {
         style={{
           position: this.style.initialCSSPosition,
           initial: this.style.initial,
+          exit: this.style.exit,
           left: this.style.initialX,
           top: this.style.initialY,
           transform: this.style.initialTransform,
           transformOrigin: this.style.initialTransformOrigin,
         }}
+        onClick={this.onClick}
         {...this.motion}
       >
         {this.children}
@@ -106,7 +123,8 @@ const AnimateBase = ({ config }: { config: AnimateConfig }): JSX.Element => {
     .setStyleProps(config.style || {})
     .setMotionProps(config.motion || {})
     .setMotionComponent(config.motionComponent || motion.div)
-    .setChildren(config.children);
+    .setChildren(config.children)
+    .setOnClick(config.onClick);
 
   return builder.build();
 };
@@ -118,14 +136,10 @@ const AnimateBase = ({ config }: { config: AnimateConfig }): JSX.Element => {
  * @param {ReactNode} [props.children] - The children nodes.
  * @returns {JSX.Element} The animated component.
  */
-const AnimateWrapper = ({
-  children,
-}: {
-  children?: ReactNode;
-}): JSX.Element => {
+const AnimateWrapper = memo(({ children }: { children?: ReactNode }): JSX.Element => {
   const WrapperAnimationConfig = getWrapperAnimationConfig();
   return <AnimateBase config={{ ...WrapperAnimationConfig, children }} />;
-};
+});
 
 /**
  * Header component for creating animated header elements.
@@ -134,18 +148,34 @@ const AnimateWrapper = ({
  * @param {ReactNode} [props.children] - The children nodes.
  * @returns {JSX.Element} The animated component.
  */
-const AnimateHeader = ({
-  children,
-}: {
-  config?: AnimateConfig;
-  children?: ReactNode;
-}): JSX.Element => {
-  const HeaderAnimationConfig = getHeaderAnimationConfig();
-  return (
-    <AnimateBase config={{ ...HeaderAnimationConfig, motionComponent: motion.header, children }} />
-  );
-};
+const AnimateHeader = memo(
+  ({ children }: { config?: AnimateConfig; children?: ReactNode }): JSX.Element => {
+    const HeaderAnimationConfig = getHeaderAnimationConfig();
+    return (
+      <AnimateBase
+        config={{ ...HeaderAnimationConfig, motionComponent: motion.header, children }}
+      />
+    );
+  },
+);
 
+const AnimateBackdrop = memo(
+  ({ children }: { config?: AnimateConfig; children?: ReactNode }): JSX.Element => {
+    const BackdropAnimationConfig = getBackdropAnimationConfig();
+    return (
+      <AnimateBase config={{ ...BackdropAnimationConfig, motionComponent: motion.div, children }} />
+    );
+  },
+);
+
+const AnimateModal = memo(
+  ({ children }: { config?: AnimateConfig; children?: ReactNode; onClick?: any }): JSX.Element => {
+    const ModalAnimationConfig = getModalAnimationConfig();
+    return (
+      <AnimateBase config={{ ...ModalAnimationConfig, motionComponent: motion.div, children, }} />
+    );
+  },
+);
 /**
  * Avatar component for creating animated avatar elements.
  * @param {Object} props - The component props.
@@ -153,15 +183,17 @@ const AnimateHeader = ({
  * @param {ReactNode} [props.children] - The children nodes.
  * @returns {JSX.Element} The animated component.
  */
-const AnimateAvatar = ({ children }: { children?: ReactNode }): JSX.Element => {
+const AnimateAvatar = memo(({ children }: { children?: ReactNode }): JSX.Element => {
   const AvatarAnimationConfig = getAvatarAnimationConfig();
   return (
     <AnimateBase config={{ ...AvatarAnimationConfig, motionComponent: motion.div, children }} />
   );
-};
+});
 
 export const Animate = {
   Wrapper: AnimateWrapper,
   Header: AnimateHeader,
   Avatar: AnimateAvatar,
+  Backdrop: AnimateBackdrop,
+  Modal: AnimateModal,
 };
