6e6d5a55a1c1973ddd2ae7236925437d2c8163c3
potocki92 <mateusz.potocki92@gmail.com>
2024-11-09 20:57:10 +0100

    feat: add Million.js plugin to Vite config
    1. Added Million.js plugin to Vite configuration.
    2. Updated plugins array to include Million.js.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import styleX from "vite-plugin-stylex";
+import MillionLint from "@million/lint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), styleX()],
+  plugins: [react(), styleX(), MillionLint.vite()],
 });
