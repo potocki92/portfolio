1a9c2d5e1a27abe4c9f86d2072220a88d113c750
potocki92 <mateusz.potocki92@gmail.com>
2024-11-09 20:52:59 +0100

    refactor: optimize ToggleTheme component with memo and useMemo
    1. Wrapped ToggleTheme component with `memo` for performance optimization.
    2. Used `useMemo` for memoizing Moon and Sun icons.

--- src/Components/ToggleTheme/ToggleTheme.tsx
+++ src/Components/ToggleTheme/ToggleTheme.tsx
@@ -2,6 +2,7 @@ import { StyleXStyles } from "@stylexjs/stylex";
 import Button from "../Button/Button";
 import { useTheme } from "../ThemeContext/ThemeContex";
 import { Sun, Moon } from "react-feather";
+import { memo, useMemo } from "react";
 const iconToggleSize: string = "1rem" as const;
 type ToggleProps = {
   style?: StyleXStyles;
@@ -10,13 +11,16 @@ type ToggleProps = {
 const ToggleTheme = ({}: React.PropsWithChildren<ToggleProps>) => {
   const { theme, toggleTheme } = useTheme();
 
+  const memoizedMoon = useMemo(() => <Moon size={iconToggleSize} />, [iconToggleSize]);
+  const memoizedSun = useMemo(() => <Sun size={iconToggleSize} />, [iconToggleSize]);
+
   return (
     <>
       <Button onClick={toggleTheme} is={true} aria-label="Toggle Theme">
-        {theme === "light" ? <Moon size={iconToggleSize} /> : <Sun size={iconToggleSize} />}
+        {theme === "light" ? memoizedMoon : memoizedSun}
       </Button>
     </>
   );
 };
 
-export default ToggleTheme;
+export default memo(ToggleTheme);
