68c8c5c476ad4836c00183a5580d60e156a03469
potocki92 <mateusz.potocki92@gmail.com>
2024-11-22 20:15:54 +0100

    feat: use Animate.ThemeButton and lucide-react icons in ToggleTheme
    1. Integrated `Animate.ThemeButton` for button animations.
    2. Used `Sun` and `Moon` icons from `lucide-react`.

--- src/Components/ToggleTheme/ToggleTheme.tsx
+++ src/Components/ToggleTheme/ToggleTheme.tsx
@@ -1,8 +1,9 @@
 import { StyleXStyles } from "@stylexjs/stylex";
 import Button from "../Button/Button";
 import { useTheme } from "../ThemeContext/ThemeContex";
-import { Sun, Moon } from "react-feather";
 import { memo, useMemo } from "react";
+import { Sun, Moon } from "lucide-react";
+import { Animate } from "../AnimateWrapper/Animation";
 const iconToggleSize: string = "1rem" as const;
 type ToggleProps = {
   style?: StyleXStyles;
@@ -10,14 +11,19 @@ type ToggleProps = {
 
 const ToggleTheme = ({}: React.PropsWithChildren<ToggleProps>) => {
   const { theme, toggleTheme } = useTheme();
-
   const memoizedMoon = useMemo(() => <Moon size={iconToggleSize} />, [iconToggleSize]);
   const memoizedSun = useMemo(() => <Sun size={iconToggleSize} />, [iconToggleSize]);
 
   return (
     <>
-      <Button onClick={toggleTheme} is={true} aria-label="Toggle Theme">
-        {theme === "light" ? memoizedMoon : memoizedSun}
+      <Button
+        onClick={toggleTheme}
+        is={true}
+        aria-label={theme === "dark" ? "Switch to light mode" : "Switch to dark mode"}
+      >
+        <Animate.ThemeButton key={theme === "dark" ? "dark" : "light"}>
+          {theme === "dark" ? memoizedSun : memoizedMoon}
+        </Animate.ThemeButton>
       </Button>
     </>
   );
