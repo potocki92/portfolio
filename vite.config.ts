33755788ec8af779824bc801859b0809a518d61e
potocki92 <mateusz.potocki92@gmail.com>
2024-11-22 20:09:40 +0100

    feat: add server configuration to Vite
    1. Added server configuration with host and port settings to Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,8 @@ import MillionLint from "@million/lint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), styleX(), MillionLint.vite()],
+  server: {
+    host: true,
+    port: 3000,
+  },
 });
