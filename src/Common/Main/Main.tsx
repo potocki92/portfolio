a98c91e89f1a32ea6b28a7f51175786ca6469763
potocki92 <mateusz.potocki92@gmail.com>
2024-12-01 19:33:39 +0100

    feat: add Social component to Main
    1. Added `Social` component to Main for displaying social media links.

--- src/Common/Main/Main.tsx
+++ src/Common/Main/Main.tsx
@@ -1,17 +1,19 @@
 import * as stylex from "@stylexjs/stylex";
 import { Outlet } from "react-router-dom";
+import { Social } from "../../Components/Social/Social";
 
 const styles = stylex.create({
   container: {
     display: "flex",
     justifyContent: "center",
-  }
+  },
 });
 const Main = () => {
   return (
     <main>
       <div {...stylex.props(styles.container)}>
         <Outlet />
+        <Social></Social>
       </div>
     </main>
   );
