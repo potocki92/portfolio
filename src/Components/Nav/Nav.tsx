37e1ffaa14257d60e1adc80e332e6eb4e369b5dc
potocki92 <mateusz.potocki92@gmail.com>
2024-12-01 19:30:22 +0100

    refactor: update Nav component with new imports and hooks
    1. Added `LanguageButton`, `ToggleTheme`, `useModal`, `t`, and `Backdrop` imports.
    2. Integrated `useModal` hook for modal state management.
    3. Used `LanguageButton` and `ToggleTheme` components.
    4. Applied `Backdrop` for modal display.

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -8,6 +8,11 @@ import Button from "../Button/Button";
 import LifeApiComponent from "../../data/lifeApi";
 import { Animate } from "../AnimateWrapper/Animation";
 import { useScrollLock } from "../../hooks/useScrollLock";
+import LanguageButton from "../LanguageButton/LanguageButton";
+import ToggleTheme from "../ToggleTheme/ToggleTheme";
+import { useModal } from "../../hooks/useModal";
+import { t } from "i18next";
+import { Backdrop } from "../Backdrop/Backdrop";
 
 interface NavigationItemsInterface {
   href: string;
@@ -42,54 +47,50 @@ const NavLink = ({ href, children }: React.PropsWithChildren<{ href: string }>) 
   );
 };
 
-const Modal = ({
+const NavModal = ({
   isModalOpen,
   setIsModalOpen,
+  toggleModal,
 }: {
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  toggleModal: () => void;
 }) => {
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
   const stopPropagation = (e: React.MouseEvent<HTMLDivElement>) => {
     e.stopPropagation();
   };
 
-  useScrollLock(isModalOpen);
   const { Navigation, NavigationText } = LifeApiComponent();
-
   const memoizedAnimatePresence = useMemo(
     () => (
-      <AnimatePresence mode="wait">
-        {isModalOpen && (
-          <Animate.Backdrop onClick={closeModal}>
-            <Animate.Modal onClick={stopPropagation}>
-              <div {...stylex.props(styles.bottomMargin, styles.container)}>
-                <Button style={styles.navButton} onClick={closeModal}>
-                  <X size={16} />
-                </Button>
-                <h2 {...stylex.props(styles.h2)}>{NavigationText}</h2>
-              </div>
-              <nav>
-                <ul {...stylex.props(styles.ul)}>
-                  {NavigationItems(Navigation).map((item, index) => (
-                    <NavItem
-                      key={item.href}
-                      href={item.href}
-                      isLast={index === Navigation.length - 1}
-                      onClick={closeModal}
-                    >
-                      {item.name}
-                    </NavItem>
-                  ))}
-                </ul>
-              </nav>
-            </Animate.Modal>
-          </Animate.Backdrop>
-        )}
-      </AnimatePresence>
+      <Backdrop isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}>
+        <Animate.Modal onClick={stopPropagation}>
+          <div {...stylex.props(styles.bottomMargin, styles.container)}>
+            <Button style={styles.navButton} onClick={toggleModal}>
+              <X size={16} />
+            </Button>
+            <h2 {...stylex.props(styles.h2)}>{NavigationText}</h2>
+          </div>
+          <nav>
+            <ul {...stylex.props(styles.ul)}>
+              {NavigationItems(Navigation).map((item, index) => (
+                <NavItem
+                  key={item.href}
+                  href={item.href}
+                  isLast={index === Navigation.length - 1}
+                  onClick={toggleModal}
+                >
+                  {item.name}
+                </NavItem>
+              ))}
+            </ul>
+            <div {...stylex.props(styles.modalButton)}>
+              <ToggleTheme />
+              <LanguageButton />
+            </div>
+          </nav>
+        </Animate.Modal>
+      </Backdrop>
     ),
     [isModalOpen, Navigation],
   );
@@ -98,18 +99,13 @@ const Modal = ({
 };
 
 export const MobileNav = memo(() => {
-  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-
-  const handleButtonClick = useCallback(() => {
-    setIsModalOpen(true);
-  }, []);
-
+  const { isModalOpen, toggleModal, setIsModalOpen } = useModal();
   return (
     <>
-      <Button onClick={handleButtonClick}>
+      <Button onClick={toggleModal}>
         <AlignJustify size={16} />
       </Button>
-      <Modal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+      <NavModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} toggleModal={toggleModal} />
     </>
   );
 });
