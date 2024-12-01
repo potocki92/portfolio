b61a3efe7051606ddb6e69f846c1cb22276f5541
potocki92 <mateusz.potocki92@gmail.com>
2024-12-01 19:26:28 +0100

    chore: remove Million.js plugin from Vite config
    1. Removed Million.js plugin from Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import styleX from "vite-plugin-stylex";
-import MillionLint from "@million/lint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), styleX(), MillionLint.vite()],
+  plugins: [react(), styleX()],
   server: {
     host: true,
     port: 3000,
