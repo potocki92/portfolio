983107612b469811f2cf738fd389baecffe3d90f
potocki92 <mateusz.potocki92@gmail.com>
2024-12-01 19:33:14 +0100

    refactor: remove ToggleTheme and LanguageButton from Header
    1. Removed `ToggleTheme` and `LanguageButton` components from Header.

--- src/Common/Header/Header.tsx
+++ src/Common/Header/Header.tsx
@@ -3,8 +3,6 @@ import { useLocation } from "react-router-dom";
 import { Nav, MobileNav } from "../../Components/Nav/Nav";
 import { Animate } from "../../Components/AnimateWrapper/Animation";
 import Avatar from "../../Components/Avatar/Avatar";
-import ToggleTheme from "../../Components/ToggleTheme/ToggleTheme";
-import LanguageButton from "../../Components/LanguageButton/LanguageButton";
 
 const Header = () => {
   const location = useLocation();
@@ -20,8 +18,6 @@ const Header = () => {
         )}
         <Nav />
         <MobileNav />
-        <ToggleTheme />
-        <LanguageButton />
       </Animate.Header>
       {isHomePage && (
         <Animate.Wrapper>
